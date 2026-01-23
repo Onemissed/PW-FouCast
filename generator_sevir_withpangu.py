a09470062ad4d076ae72ac48f554beb37d0c80d9
Onemissed <13501145338@163.com>
2026-01-23 13:25:19 +0800

    Second commit

--- generator_sevir_withpangu.py
+++ generator_sevir_withpangu.py
@@ -6,17 +6,16 @@ class DataGenerator(Dataset):
     def __init__(self, lists, pangu_list):
         self.lists = lists
         self.pangu_list = pangu_list
-        # self.root_path = root_path
 
     def __len__(self):
         return len(self.lists)
 
     def __getitem__(self, index):
         if 'random' in str(self.lists[index]):
-            sequence = np.load(os.path.join('/media/qyz/lightning_data/sevir_lr/data/vil_single/random/', str(self.lists[index]) + '.npy'))
+            sequence = np.load(os.path.join('/path/to/weather_data/sevir_lr/data/vil_single/random/', str(self.lists[index]) + '.npy'))
         else:
-            sequence = np.load(os.path.join('/media/qyz/lightning_data/sevir_lr/data/vil_single/storm', str(self.lists[index]) + '.npy'))
+            sequence = np.load(os.path.join('/path/to/weather_data/sevir_lr/data/vil_single/storm', str(self.lists[index]) + '.npy'))
 
-        pangu_sequence = np.load(os.path.join('/media/qyz/lightning_data/pangu_weather/pangu_interpolate', str(self.pangu_list[index]) + '.npy'))
+        pangu_sequence = np.load(os.path.join('/path/to/weather_data/pangu_weather/pangu_interpolate', str(self.pangu_list[index]) + '.npy'))
 
         return sequence, pangu_sequence
